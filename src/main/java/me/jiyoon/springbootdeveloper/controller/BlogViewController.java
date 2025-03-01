b8e295a33ef4793f30f12e7a6aa7f7a7c30e91a1
y11n <samtori3849@gmail.com>
2025-03-01 15:02:09 +0900

    [Feat] 블로그 목록 뷰 구현

---
+++ src/main/java/me/jiyoon/springbootdeveloper/controller/BlogViewController.java
@@ -0,0 +1,27 @@
+package me.jiyoon.springbootdeveloper.controller;
+
+import lombok.RequiredArgsConstructor;
+import me.jiyoon.springbootdeveloper.dto.ArticleListViewResponse;
+import me.jiyoon.springbootdeveloper.service.BlogService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Controller
+public class BlogViewController {
+
+    private final BlogService blogService;
+
+    @GetMapping("/articles")
+    public String getArticles(Model model) {
+        List<ArticleListViewResponse> articles = blogService.findAll().stream()
+                .map(ArticleListViewResponse :: new)
+                .toList();
+        model.addAttribute("articles", articles);
+
+        return "articleList";
+    }
+}
