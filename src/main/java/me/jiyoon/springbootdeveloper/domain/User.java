cd8638183810c4e643d42fbbf001d9debd81477c
y11n <samtori3849@gmail.com>
2025-03-03 00:15:40 +0900

    [Feat] 회원 도메인 생성

--- src/main/java/me/jiyoon/springbootdeveloper/domain/User.java
+++ src/main/java/me/jiyoon/springbootdeveloper/domain/User.java
@@ -1,2 +1,77 @@
-package me.jiyoon.springbootdeveloper.domain;public class User {
+package me.jiyoon.springbootdeveloper.domain;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@Table(name = "users")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@Entity
+public class User implements UserDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", updatable = false)
+    private Long id;
+
+    @Column(name = "email", nullable = false, unique= true)
+    private String email;
+
+    @Column(name = "password")
+    private String password;
+
+    @Builder
+    public User (String email, String password){
+        this.email = email;
+        this.password = password;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("user"));
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        // 만료되었는지 확인하는 로직
+        return false; // true -> 잠금되지 않음
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        // 계정 잠금되었는지 확인하는 로직
+        return false; // true -> 잠금되지 않음
+    }
+
+    // 패스워드 만료 여부 반환
+    @Override
+    public boolean isCredentialsNonExpired() {
+        // 패스워드 만료 여부 확인하는 로직
+        return false; // true -> 잠금되지 않음
+    }
+
+    @Override
+    public boolean isEnabled() {
+        // 계정 사용 가능한지 확인하는 로직
+        return false; // true -> 사용 가능
+    }
 }
---
+++ src/main/java/me/jiyoon/springbootdeveloper/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package me.jiyoon.springbootdeveloper.repository;
+
+import me.jiyoon.springbootdeveloper.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+}
