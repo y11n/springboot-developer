7151f9f4f4f06d25d3b76fa6447e03ea886975dd
y11n <samtori3849@gmail.com>
2025-03-04 17:43:38 +0900

    [Feat] 리프레시 토큰 재발급 API 추가

---
+++ src/main/java/me/jiyoon/springbootdeveloper/controller/TokenApiController.java
@@ -0,0 +1,26 @@
+package me.jiyoon.springbootdeveloper.controller;
+
+import lombok.RequiredArgsConstructor;
+import me.jiyoon.springbootdeveloper.dto.CreateAccessTokenRequest;
+import me.jiyoon.springbootdeveloper.dto.CreateAccessTokenResponse;
+import me.jiyoon.springbootdeveloper.service.TokenService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+public class TokenApiController {
+    private final TokenService tokenService;
+
+    @PostMapping("/api/token")
+    public ResponseEntity<CreateAccessTokenResponse> createNewAccessToken(
+            @RequestBody CreateAccessTokenRequest request){
+        String newAccessToken = tokenService.createNewAccessToken(request.getRefreshToken());
+
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(new CreateAccessTokenResponse(newAccessToken));
+    }
+}
